39c3f44a5a7331338b88752fa616e3478b323820
mdzarif55 <muhtasimzarif55@gmail.com>
2025-08-03 04:44:21 +0600

    log in created without submit

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -4,12 +4,13 @@ import { assets, features } from '../assets/assets'
 const BottomBanner = () => {
     return (
         <div className='relative mt-24'>
-            <img src={assets.bottom_banner_image} alt="Banner" className='w-full hidden md:block' />
-            <img src={assets.bottom_banner_image_sm} alt="Banner" className='w-full md:hidden' />
+            <img src={assets.bottom_banner_image} alt="Banner" className='w-full hidden
+            md:block' />
+            <img src={assets.bottom_banner_image_sm} alt="Banner" className='w-full  md:hidden' />
 
-            <div className=' absolute  inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24 '>
+            <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
                 <div>
-                    <h1 className='text-2xl md:w-3xl font-semibold text-primary mb-6' >Why We Are the Best?</h1>
+                    <h1 className='text-2xl md:text-3xl font-semibold text-primary' >Why We Are the Best?</h1>
                     {features.map((feature, index) => (
                         <div key={index} className='flex items-center gap-4 mt-2' >
                             <img src={feature.icon} alt={feature.title} className='md:w-11 w-9' />
