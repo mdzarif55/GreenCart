4bfef5c6252bf890c3a6f19bbad7cae959b39444
mdzarif55 <muhtasimzarif55@gmail.com>
2025-08-03 03:59:37 +0600

    footer done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Catagories from '../components/Catagories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,8 @@ const Home = () => {
         <Catagories/>
         <BestSeller/>
         <BottomBanner/>
+        <NewsLetter/>
+        
     </div>
   )
 }
