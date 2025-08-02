e4cbd3d313297aa101b046d7f4e2950451c47ab2
mdzarif55 <muhtasimzarif55@gmail.com>
2025-08-02 22:33:01 +0600

    psuhing

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Catagories from '../components/Catagories'
+import BestSeller from '../components/BestSeller'
+
+const Home = () => {
+  return (
+    <div className=' mt-10'>
+        <MainBanner/>
+        <Catagories/>
+        <BestSeller/>
+    </div>
+  )
+}
+
+
+export default Home
