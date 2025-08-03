34da4d4992bf4db03883812144c7e78d5a12e332
mdzarif55 <muhtasimzarif55@gmail.com>
2025-08-04 04:13:34 +0600

    cart page sucessfylly done with logic

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,23 +5,23 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery } = useAppContext();
+    const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery, getCartCount } = useAppContext();
 
     const logout = async () => {
         setUser(null);
         navigate('/')
     }
 
-    useEffect(()=>{
-        if(searchQuery.length > 0 ){
+    useEffect(() => {
+        if (searchQuery.length > 0) {
             navigate("/products")
         }
-    },[searchQuery])
+    }, [searchQuery])
 
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to='/' onClick={()=>setOpen(false)}>
+            <NavLink to='/' onClick={() => setOpen(false)}>
                 <img className="h-9" src={assets.logo} alt="logo" />
             </NavLink>
 
@@ -32,13 +32,13 @@ const Navbar = () => {
                 <NavLink to='/'>Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input onChange={(e)=> setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <img src={assets.search_icon} alt="search" className='w-4 h-4' />
                 </div>
 
-                <div onClick={()=>navigate("/cart")} className="relative cursor-pointer">
+                <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ? (
@@ -57,11 +57,17 @@ const Navbar = () => {
                     )
                 }
             </div>
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt="cart" className='w-6 opacity-80' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
+                    {/* Menu Icon SVG */}
+                    <img src={assets.menu_icon} alt='menu' />
+                </button>
+            </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
-                {/* Menu Icon SVG */}
-                <img src={assets.menu_icon} alt='menu' />
-            </button>
 
             {/* Mobile Menu */}
             {
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContext = createContext();
 export const AppContextProvider = ({children})=>{
 
 
-    const currency = import.meta.VITE_CURRENCY;
+    const currency = import.meta.env.VITE_CURRENCY;
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setSeller] = useState(false);
@@ -60,11 +60,34 @@ export const AppContextProvider = ({children})=>{
         setCartItems(cartData);
     }
 
+    //Get Cart Count 
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const item in cartItems){
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    //Get cart total ammount
+    
+    const getCartAmount = ()=>{
+        let totalAmount = 0;
+        for(const  items in cartItems){
+            let itemInfo = products.find((product)=> product._id === items);
+            if(cartItems[items] > 0){
+                totalAmount+=itemInfo.offerPrice * cartItems[items]
+            }
+
+        }
+        return Math.floor(totalAmount *100)/100;
+    }
+
     useEffect(()=> {
         fetchProducts()
     },[])
 
-    const value = { navigate, user, setUser , setSeller, isSeller, showUserLogin,setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery}
+    const value = { navigate, user, setUser , setSeller, isSeller, showUserLogin,setShowUserLogin, products, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery, getCartAmount, getCartCount}
 
 
     return <AppContext.Provider value={value} >
